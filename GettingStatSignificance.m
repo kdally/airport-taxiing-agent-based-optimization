dbc87c99c1bda2fc4605cb5592e27a74363ecec0
killian <k.dally@student.tudelft.nl>
2019-11-21 17:05:30 +0100

    Coeff of Variation

--- GettingStatSignificance.m
+++ GettingStatSignificance.m
@@ -1,13 +1,17 @@
 
-%% MODEL ANALYSIS. JUST CHANGE FILE NAME LINE 5 and 30
+%% MODEL ANALYSIS. JUST CHANGE FILE NAME LINE 5 and 30. 
+% output has mean values on first three rows, below row of zeros, below
+% coefficient of variation
+% 1000 for coeff. of variation means no variation
 
 clear
-raw_table = readtable('Results/Assignment2_basic experiment-table-or.csv');
+raw_table = readtable('Results/Assignment2_basic experiment-table-V1.csv');
+%raw_table = readtable('Results/Assignment2_basic experiment-table-or.csv');
 raw_cell = table2cell(raw_table(:,4:end));
 
-% IF ERROR, UNCOMMENT LINE 9 AND COMMENT LINE 10
-%all_conditions = str2double(raw_cell);
-all_conditions = cell2mat(raw_cell);
+% IF ERROR, COMMENT LINE 9 AND UNCOMMENT LINE 10
+all_conditions = str2double(raw_cell);
+%all_conditions = cell2mat(raw_cell);
 
 normal = all_conditions(1:10,:);
 high = all_conditions(11:20,:);
@@ -16,21 +20,20 @@ asym = all_conditions(21:30,:);
 clear raw_table
 clear raw_cell
 clear all_conditions
-coeff_normal = array2table(std(normal)./mean(normal));
+coeff_normal = (std(normal)./mean(normal));
+coeff_normal(isnan(coeff_normal)) = 0;
 mean_normal = mean(normal);
 
-coeff_high = array2table(std(high)./mean(high));
+coeff_high = (std(high)./mean(high));
+coeff_high(isnan(coeff_high)) = 0;
 mean_high = mean(high);
 
-coeff_asym = array2table(std(asym)./mean(asym));
+coeff_asym = (std(asym)./mean(asym));
+coeff_asym(isnan(coeff_asym)) = 0;
 mean_asym = mean(asym);
 
-output = [mean_normal; mean_high; mean_asym]; 
+output = [mean_normal; mean_high; mean_asym; zeros(size(coeff_high)); coeff_normal; coeff_high; coeff_asym];
 
-%output2 = [coeff_normal; coeff_high, coeff_asym];
-
-xlswrite('Results/orginal_model.xlsx',output)
-
-
-
+csvwrite('Results/V1_model.csv',output)
+%csvwrite('Results/orginal_model.csv',output)
 
